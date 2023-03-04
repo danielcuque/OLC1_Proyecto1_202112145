bcbf39f7653879890114e255b7af1ef75de0405c
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-03-03 21:22:14 -0600

    refactor: improve graph tree method

--- src/main/java/com/daniel/model/ReportGraphviz.java
+++ src/main/java/com/daniel/model/ReportGraphviz.java
@@ -19,40 +19,37 @@ public class ReportGraphviz {
         mf.WriteFiles(path, content);
     }
 
-    public String generateTreeGraph(Node root, String nameRegex){
-        String graph = "";
+    public String generateTreeGraph(Node root, String nameRegex) {
         if (root == null) {
             return "";
         }
-        if (root.number == 0){
-            graph += "digraph "+ nameRegex +"{\n";
-            graph += "root" + root.number + "[label=\"" + root.lexeme + "\"];\n";
-            graph += "root" + root.number + " ->"+root.left.type + root.left.number + ";\n";
-            graph += "root" + root.number + " ->"+root.right.type + root.right.number + ";\n";
 
-            graph += generateTreeGraph(root.left, nameRegex);
-            graph += generateTreeGraph(root.right, nameRegex);
-            graph += "}";
-            return graph;
+        StringBuilder sb = new StringBuilder();
+        if (root.number == 0) {
+            sb.append(String.format("digraph %s {\n", nameRegex));
+            sb.append(String.format("root%d [label=\"%s\"];\n", root.number, root.lexeme));
+            sb.append(String.format("root%d ->%s%d;\n", root.number, root.left.type, root.left.number));
+            sb.append(String.format("root%d ->%s%d;\n", root.number, root.right.type, root.right.number));
+
+            sb.append(generateTreeGraph(root.left, nameRegex));
+            sb.append(generateTreeGraph(root.right, nameRegex));
+            sb.append("}");
+            return sb.toString();
         }
-        switch (root.type) {
-            case LEAVE, ACCEPT -> {
-                graph +=root.type + "" + root.number + "[label=\"" + root.lexeme + "\"];\n";
-            }
-            case AND, OR -> {
-                graph += root.type + "" + root.number + " [label=\"" + root.lexeme + "\"];\n";
-                graph += root.type + "" + root.number + " ->" + root.left.type + root.left.number + ";\n";
-                graph += root.type + "" + root.number + " ->" + root.right.type + root.right.number + ";\n";
-                graph += generateTreeGraph(root.left, nameRegex);
-                graph += generateTreeGraph(root.right, nameRegex);
-            }
-            case STAR -> {
-                graph += root.type + "" + root.number + " [label=\"" + root.lexeme + "\"];\n";
-                graph += root.type + "" + root.number + " ->" + root.left.type + root.left.number + ";\n";
-                graph += generateTreeGraph(root.left, nameRegex);
-            }
-        }
-        return graph;
+
+        String label = root.lexeme.toString();
+        String typeLabel = switch (root.type) {
+            case LEAVE, ACCEPT -> String.format("%s%d [label=\"%s\"];\n", root.type, root.number, label);
+            case AND, OR -> String.format("%s%d [label=\"%s\"];\n%s%d ->%s%d;\n%s%d ->%s%d;\n",
+                    root.type, root.number, label, root.type, root.number, root.left.type, root.left.number,
+                    root.type, root.number, root.right.type, root.right.number);
+            case STAR -> String.format("%s%d [label=\"%s\"];\n%s%d ->%s%d;\n",
+                    root.type, root.number, label, root.type, root.number, root.left.type, root.left.number);
+            default -> "";
+        };
+        sb.append(typeLabel);
+        sb.append(generateTreeGraph(root.left, nameRegex));
+        sb.append(generateTreeGraph(root.right, nameRegex));
+        return sb.toString();
     }
-
 }
